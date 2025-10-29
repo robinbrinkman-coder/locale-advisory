53a5859bfa402f4e9a5dab1cdcd2a382723277ef
robinbrinkman-coder <robinbrinkman@gmail.com>
2025-10-29 18:07:21 +1100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
+  },
 });
