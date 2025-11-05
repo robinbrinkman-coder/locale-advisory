47483aed3dbcd8a931a56ef14ce4602b12be7b89
robinbrinkman-coder <robinbrinkman@gmail.com>
2025-11-05 17:14:55 +1100

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,7 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './styles/globals.css';
+import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
